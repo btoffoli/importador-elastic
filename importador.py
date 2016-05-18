d710ea20a278fff51c533d949e7cae14d9e80a6a
Bruno R. Toffoli <btoffoli@gmail.com>
2016-05-17 22:58:17 -0300

    change just sql to import the data

--- importador.py
+++ importador.py
@@ -6,9 +6,12 @@ import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from elasticsearch import Elasticsearch
 from tide import TideService
+from tide_connection import TideConnection
+
+
 class ImportadorElastic:
-
-
+    SQL_QUERY_COUNT = 'SELECT COUNT(1) FROM ocorrencia'
+    SQL_QUERY_OCORRENCIA = 'SELECT o.* FROM ocorrencia o ORDER BY data_hora_criacao ASC LIMIT :limit OFFSET :offset'
 
     def __init__(self, numMaxThreads, nomeDoTipoDocumento, nomeDoIndice):
         self.fila = deque()
@@ -17,22 +20,28 @@ class ImportadorElastic:
         self.elastic = Elasticsearch()
         self.nomeDoTipoDocumento = nomeDoTipoDocumento
         self.nomeDoIndice = nomeDoIndice
-        self.tideService = TideService()
-
 
     def iniciarServico(self):
-        #self.currentThreadGerenciadoraDeFila = Thread(daemon=True, target=self.importarRegistros)
+        # self.currentThreadGerenciadoraDeFila = Thread(daemon=True, target=self.importarRegistros)
         self.importarRegistros()
-
 
     def importarRegistros(self):
         fetchSize = 100
-        totalOcorrencias = self.tideService.countOcorrencias()
+        # totalOcorrencias = self.tideService.countOcorrencias()
+        con = TideConnection().getConn()
+        cur = con.cursor()
+
+        cur.execute(self.SQL_QUERY_COUNT)
+        totalOcorrencias = cur.fetchone()[0]
 
         with ThreadPoolExecutor(max_workers=self.numMaxThreads) as executor:
-            for page in range(1..ceil(totalOcorrencias/fetchSize)):
-                #constroi gerador de future inserindo ocorrencia
-                ocorrencias = self.tideService.listOcorrencias(page * fetchSize)
+            total_paginas = (ceil(totalOcorrencias / fetchSize))
+            for page in range(1, total_paginas):
+                # constroi gerador de future inserindo ocorrencia
+                con.commit()
+                cur.execute(self.SQL_QUERY_OCORRENCIA.replace(':limit', str(fetchSize)).replace(':offset',
+                                                                                                str(fetchSize * page)))
+                ocorrencias = cur.fetchall()
                 future_generator_ocorrencia = {executor.submit(self.__importar, o): o for o in ocorrencias}
                 while (not as_completed(future_generator_ocorrencia)):
                     time.sleep(3)
@@ -40,9 +49,10 @@ class ImportadorElastic:
     def __importar(self, registro):
         t = current_thread()
         try:
+            # print("registro - %s \n" % (str(registro)))
             self.elastic.index(doc_type=self.nomeDoTipoDocumento, index=self.nomeDoIndice, body=registro)
-        except:
-            print("Ocorreu um erro com a thread {}, no registro {}".format(t, registro))
+        except Exception as e:
+            print("Ocorreu um erro com a thread {}, no registro {} - {}\n".format(t, registro, e))
 
 
 if __name__ == '__main__':
--- tide_connection.py
+++ tide_connection.py
@@ -1,16 +1,21 @@
 __author__ = 'btoffoli'
 
 from sqlalchemy import create_engine
+from psycopg2 import connect as connection
 
 class TideConnection:
 
+    #def __init__(self):
+    #    self.__engineCurrent = None
+
+    #def getEngine(self):
+    #    if not self.__engineCurrent:
+    #        self.__engineCurrent = create_engine("postgresql://geocontrol:geo007@localhost/tide")
+    #    return self.__engineCurrent
+
     def __init__(self):
-        self.__engineCurrent = None
+        self.__conn = None
 
-    def getEngine(self):
-        if not self.__engineCurrent:
-            self.__engineCurrent = create_engine("postgresql://user:password@localhost/tide")
-        return self.__engineCurrent
-
-
+    def getConn(self):
+        return connection("dbname='tide' user='geocontrol' host='localhost' password='geo007'")
 
